883b1c48e402cc07491124a819e3899800942ff1
Yan Wu <yenwu.tw@gmail.com>
2022-05-21 11:26:41 +0800

    ch2: install sh & config.txt

---
+++ chapter_2/install.sh
@@ -0,0 +1,8 @@
+# $ lsb_release -a
+# No LSB modules are available.
+# Distributor ID: Debian
+# Description:    Debian GNU/Linux 11 (bullseye)
+# Release:        11
+# Codename:       bullseye
+sudo apt-get update
+sudo apt-get install -y libncurses5-dev gcc-aarch64-linux-gnu build-essential git bison flex libssl-dev libfdt-dev
